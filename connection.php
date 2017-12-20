3b4c785552b706d62e6845deea41db1b13d1dd90
Unknown <dewantysatya@ymail.com>
2017-12-20 18:33:07 +0800

    Connection

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "wildwood";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+
+?>
