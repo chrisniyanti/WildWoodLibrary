2ae0182e70e22d7b2c78b1b6835bf8ac9c8854fc
Unknown <chrisayu7@gmail.com>
2017-12-27 16:52:46 +0800

    register Exe

---
+++ registerExe.php
@@ -0,0 +1,30 @@
+<?php 
+    include 'connection.php';
+        if(isset($_POST['add'])){
+        	$name        = $_POST['name'];
+            $username    = $_POST['username'];
+            $email       = $_POST['email'];
+        	$password    = md5($_POST['password']);
+            $address     = $_POST['address'];
+            $birthday    = $_POST['birthday'];
+        	$gender      = $_POST['gender'];
+        	$phonenumber = $_POST['phonenumber'];
+        
+            $dir        ="upload/";
+            $extension  =$_FILES["photo_admin"]["type"];
+            $filename   =$_FILES["photo_admin"]["name"];
+            $filesize   =$_FILES["photo_admin"]["size"];
+            $tmpname    =$_FILES["photo_admin"]["tmp_name"];
+            $finalup    = $dir.$filename;
+            if($extension=="image/jpeg" or $extension=="image/png"){
+                if(move_uploaded_file($tmpname,$finalup)){
+        	       $query = "INSERT INTO tb_admin VALUES('', '$name', '$username', '$email', '$password', '$address', '$birthday', '$gender', '$phonenumber', '$finalup')";
+        	       mysqli_query($conn, $query);
+            echo "
+            <script>
+                alert('Your Data Have Been Submitted Successfully!');location.href='index.php';
+            </script>"; 
+                }
+            }
+        }
+ ?>
