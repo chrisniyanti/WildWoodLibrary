e3f9eff31e58e785258bdf0cd264ecc9a4af6a66
Unknown <chrisayu7@gmail.com>
2017-12-27 16:51:16 +0800

    update new index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Sign in</title><link rel="shortcut icon" href="img/TheA.png">
+		<title>Sign in</title><link rel="shortcut icon" href="img/logo.png">
 		<link rel="stylesheet" type="text/css" href="css/login.css">
 
 	</head>
@@ -14,30 +14,23 @@
 	<body>
 		<div class="wrapper fadeInDown">
   			<div id="formContent">
-		     <!-- Tabs Titles -->
 			    <h2 class="active"> Sign In </h2>
-
-			    <!-- Icon -->
-			    <div class="fadeIn first">
-			      <img src="img/logo.png" id="icon" alt="User Icon" />
-			    </div>
-
-			    <!-- Login Form -->
-			    <form action="login.php" method="POST">
-			      <input type="text" class="fadeIn second" name="username" placeholder="username" required>
-			      <input type="password" class="fadeIn third" name="passwords" placeholder="passwords" required>
-			      <input type="submit" class="fadeIn fourth" name="" value="Log In">
-			    </form>
-
-
-			    <!-- Remind Passowrd -->
-			    <div id="formFooter">
-			      <a class="underlineHover" href="#">Forgot Password?</a>
-			    </div>
-
-			  </div>
-			</div>	
-
+			    	<div class="fadeIn first">
+			      		<img src="img/logo.png" id="icon" alt="User Icon" />
+			    	</div>
+				    <form action="login.php" method="POST">
+					      <input type="text" class="fadeIn second" name="username" placeholder="username" required>
+					      <input type="password" class="fadeIn third" name="passwords" placeholder="passwords" required>
+					      <input type="submit" class="fadeIn fourth" name="" value="Log In">
+				    </form>
+				    <form action="register.php" method="POST">
+					       <div id="formFooter">
+				      	<a class="underlineHover" href="register.php">Registration?</a>
+				    </div>
+				    </form>
+				   
+			 </div>
+		</div>	
 	</body>
 </html>
 
